6ce079d262bb5a5daee4adb9e79559fde734f307
Thomas Gerot <tjgerot@cox.net>
2016-03-02 23:40:01 -0600

    Changed to the greatest font

--- UI/assets/js/slides.js
+++ UI/assets/js/slides.js
@@ -13,3 +13,20 @@ function setImageThree() {
     var imageUrl = "assets/images/slideshow/christmas.jpg";
     $('#slides').fadeIn(700).css('background-image', 'url(' + imageUrl + ')').delay(10000).fadeOut(700, function () { setImageOne(); });
 }
+
+var hashTagActive = "";
+$(".scroll").click(function (event) {
+    if(hashTagActive != this.hash) {
+        event.preventDefault();
+        var dest = 0;
+        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+            dest = $(document).height() - $(window).height();
+        } else {
+            dest = $(this.hash).offset().top;
+        }
+        $('html,body').animate({
+            scrollTop: dest
+        }, 2000, 'swing');
+        hashTagActive = this.hash;
+    }
+});

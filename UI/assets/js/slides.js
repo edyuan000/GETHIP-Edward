5f6e2178b92651a2261337abb6b18c4825704511
Thomas Gerot <tjgerot@cox.net>
2016-03-02 22:50:32 -0600

    Rewrote entire slideshow

---
+++ UI/assets/js/slides.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    setImageOne();
+});
+function setImageOne() {
+    var imageUrl = "assets/images/slideshow/halloween.jpg";
+    $('#slides').fadeIn(700).css('background-image', 'url(' + imageUrl + ')').delay(10000).fadeOut(700, function () { setImageTwo(); });
+}
+function setImageTwo() {
+    var imageUrl = "assets/images/slideshow/valentines.jpg";
+    $('#slides').fadeIn(700).css('background-image', 'url(' + imageUrl + ')').delay(10000).fadeOut(700, function () { setImageThree(); });
+}
+function setImageThree() {
+    var imageUrl = "assets/images/slideshow/christmas.jpg";
+    $('#slides').fadeIn(700).css('background-image', 'url(' + imageUrl + ')').delay(10000).fadeOut(700, function () { setImageOne(); });
+}
